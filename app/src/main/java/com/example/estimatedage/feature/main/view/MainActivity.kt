592de0d4b69dad006e56aba7e291acd644cf9980
Hugo Jordan <hugoj8366@gmail.com>
2024-05-10 12:17:16 -0300

    Finalizando interface gráfica

--- app/src/main/java/com/example/estimatedage/feature/main/view/MainActivity.kt
+++ app/src/main/java/com/example/estimatedage/feature/main/view/MainActivity.kt
@@ -3,17 +3,45 @@ package com.example.estimatedage.feature.main.view
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.estimatedage.R
 import com.example.estimatedage.ui.theme.EstimatedAgeTheme
 
 class MainActivity : ComponentActivity() {
+
+    var name: MutableState<TextFieldValue> = mutableStateOf(TextFieldValue(""))
+    var textResult: MutableState<String> = mutableStateOf("")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -21,28 +49,80 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color.White
                 ) {
                     App()
                 }
             }
         }
     }
-}
 
-@Composable
-fun App() {
-    Box(modifier = Modifier.fillMaxSize()
 
-    ) {
+    @Composable
+    fun App() {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
+            ){
+                TextField(
+                    value = name.value,
+                    onValueChange = { text ->
+                        name.value = text
+                    },
+                    modifier = Modifier
+                        .width(320.dp)
+                        .fillMaxHeight()
+                        .padding(start = 30.dp, end = 10.dp),
+                    shape = RoundedCornerShape(10.dp),
+                    maxLines = 1,
+                )
+                Image(
+                    painter = painterResource(id = R.drawable.search),
+                    contentDescription = "Search image",
+                    modifier = Modifier
+                        .background(
+                            Color.Red,
+                            shape = RoundedCornerShape(10.dp)
+                        )
+                        .fillMaxHeight()
+                        .aspectRatio(1f)
+                        .padding(10.dp)
+                        .clickable {
+                            textResult.value = name.value.text
+                        }
+                )
+            }
 
+            Text(
+                text = textResult.value,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp)
+            )
+        }
     }
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EstimatedAgeTheme {
-        App()
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        EstimatedAgeTheme {
+            EstimatedAgeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color.White
+                ) {
+                    App()
+                }
+            }
+        }
     }
 }
